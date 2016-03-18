06aeb262c6e785fd795b5f72e6f639ff1a724488
Thorsteinn Benediktsson <thorsteinn15@ru.is>
2016-03-18 17:00:12 +0000

    fhdfhf

--- Inntimatafla(var).php
+++ Inntimatafla(var).php
@@ -1,3 +1,4 @@
 <?php
 $OESiteUploadDirectory = "Upload";
 $OEConfWECheckBox = '{"WE24766379a1":{"InputName":"WE24766379a1"},"WE5420829ccd":{"InputName":"WE5420829ccd"},"WE9db332d602":{"InputName":"WE9db332d602"},"WE967f903dff":{"InputName":"WE967f903dff"},"WE415b21c46c":{"InputName":"WE415b21c46c"},"WE92c6a20b1a":{"InputName":"WE92c6a20b1a"},"WE58d4b80c19":{"InputName":"WE58d4b80c19"},"WEf488122098":{"InputName":"WEf488122098"},"WE07f9c7ec84":{"InputName":"WE07f9c7ec84"},"WEafe7fdc30b":{"InputName":"WEafe7fdc30b"},"WE16550ddb9e":{"InputName":"WE16550ddb9e"}}';
+$OEConfWEListBox = '{"WEcef0497652":{"InputName":"WEcef0497652"}}';

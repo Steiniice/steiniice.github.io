365cf2a3977d079985a785891e49dedb6507335d
Thorsteinn Benediktsson <thorsteinn15@ru.is>
2016-03-18 16:57:46 +0000

    lalal

--- Timatafla(var).php
+++ Timatafla(var).php
@@ -1,3 +1,4 @@
 <?php
 $OESiteUploadDirectory = "Upload";
 $OEConfWECheckBox = '{"WE83d98a9e06":{"InputName":"WE83d98a9e06"},"WEb041caec62":{"InputName":"WEb041caec62"},"WE4f1410d996":{"InputName":"WE4f1410d996"},"WE7a0f11bbb0":{"InputName":"WE7a0f11bbb0"},"WE9a9ab8d6bb":{"InputName":"WE9a9ab8d6bb"},"WE57d4ea9e7b":{"InputName":"WE57d4ea9e7b"},"WEe3a9081be1":{"InputName":"WEe3a9081be1"},"WEdc5d34cf92":{"InputName":"WEdc5d34cf92"},"WEe931642404":{"InputName":"WEe931642404"},"WEfb35fc0a2e":{"InputName":"WEfb35fc0a2e"},"WEaac7cddb6d":{"InputName":"WEaac7cddb6d"}}';
+$OEConfWEListBox = '{"WEedc4b9bf6e":{"InputName":"WEedc4b9bf6e"}}';
